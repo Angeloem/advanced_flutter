b23416463021e0af0c4529506336b7ab95368870
angeloem <esanga530@gmail.com>
2019-10-15 16:05:07 -0400

    imported the image to the application

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,6 @@ class MyApp extends StatelessWidget {
 		return MaterialApp(
 			title: 'Flutter Login UI',
 			theme: ThemeData(
-				// This is the theme of your application.
-				//
-				// Try running your application with "flutter run". You'll see the
-				// application has a blue toolbar. Then, without quitting the app, try
-				// changing the primarySwatch below to Colors.green and then invoke
-				// "hot reload" (press "r" in the console where you ran "flutter run",
-				// or simply save your changes to "hot reload" in a Flutter IDE).
-				// Notice that the counter didn't reset back to zero; the application
-				// is not restarted.
 				primarySwatch: Colors.blue,
 			),
 			home: MyHomePage(title: 'Flutter Login'),
@@ -27,15 +18,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
 	MyHomePage({Key key, this.title}) : super(key: key);
-	
-	// This widget is the home page of your application. It is stateful, meaning
-	// that it has a State object (defined below) that contains fields that affect
-	// how it looks.
-	
-	// This class is the configuration for the state. It holds the values (in this
-	// case the title) provided by the parent (in this case the App widget) and
-	// used by the build method of the State. Fields in a Widget subclass are
-	// always marked "final".
 	
 	final String title;
 	
@@ -61,13 +43,14 @@ class _MyHomePageState extends State<MyHomePage> {
 		decoration: InputDecoration(
 			contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
 			hintText: 'Password',
+			hasFloatingPlaceholder: true,
 			border: OutlineInputBorder(borderRadius: BorderRadius.circular(32))
 		),
 	);
     final loginButton = Material(
 		elevation: 5,
 		borderRadius: BorderRadius.circular(30),
-		color: Color(0xff01A0C7),
+		color: Color(0xff01A0d1),
 		child: MaterialButton(
 			minWidth: MediaQuery.of(context).size.width,
 			padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
